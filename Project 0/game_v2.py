26fd6892448a24b742af5b83a72fd49d95d4760a
Rioder <rioder200@mail.ru>
2022-02-24 21:58:53 +0300

    Edit

--- Project 0/game_v2.py
+++ Project 0/game_v2.py
@@ -11,7 +11,7 @@ def random_predict(number:int=1) -> int:
     """
     
     count = 0
-    low = 0
+    low = 0 
     high = 100
     while True:
         count += 1
@@ -26,7 +26,7 @@ def random_predict(number:int=1) -> int:
     
 
 def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+    """За какое количество попыток в среднем из 100 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -37,7 +37,7 @@ def score_game(random_predict) -> int:
 
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    random_array = np.random.randint(1, 101, size=(100)) # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
